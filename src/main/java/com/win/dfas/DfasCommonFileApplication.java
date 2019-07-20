c1b38e7e32e1f29d59ba067a4bcaa09e396e4580
zhongyuqi <zhongyuqi@ysstech.com>
2019-07-20 16:13:58 +0800

    [modify-问题修复] 问题原因: 解决方法: 影响范围: BugID   :

--- src/main/java/com/win/dfas/DfasCommonFileApplication.java
+++ src/main/java/com/win/dfas/DfasCommonFileApplication.java
@@ -1,11 +1,16 @@
 package com.win.dfas;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = {"com.win"})
+@EnableFeignClients(basePackages = {"com.win"})
 public class DfasCommonFileApplication {
 
 	public static void main(String[] args) {

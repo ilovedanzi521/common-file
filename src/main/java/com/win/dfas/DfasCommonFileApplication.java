3600737f950da08d6d441a1484a44bea662a153e
wanglei <wanglei@ysstech.com>
2019-08-21 17:23:38 +0800

    [modify-问题修复] 问题原因:文件服务后台代码 解决方法: 影响范围: BugID   :

--- src/main/java/com/win/dfas/DfasCommonFileApplication.java
+++ src/main/java/com/win/dfas/DfasCommonFileApplication.java
@@ -1,13 +1,21 @@
 package com.win.dfas;
 
 
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.MongoDbFactory;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 @EnableSwagger2
 @ComponentScan(basePackages = {"com.win"})
 @EnableFeignClients(basePackages = {"com.win"})
@@ -16,5 +24,12 @@ public class DfasCommonFileApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DfasCommonFileApplication.class, args);
 	}
+	@Autowired
+	private MongoDbFactory mongoDbFactory;
 
+	@Bean
+	public GridFSBucket getGridFSBuckets() {
+		MongoDatabase db = mongoDbFactory.getDb();
+		return GridFSBuckets.create(db);
+	}
 }
